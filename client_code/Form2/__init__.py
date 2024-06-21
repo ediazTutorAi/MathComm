c9aa6135c051558d32acde62f1d5249228e83f86
esd06007@gmail.com <esd06007@gmail.com>
2024-06-21 03:49:38 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -45,10 +45,10 @@ class Form2(Form2Template):
 
   def check_type_of_user(self):
     user = anvil.users.get_user()
-    if user['role']=='student':
-      return 'student'
-    else:
+    if user['role']=='instructor':
       return 'instructor'
+    else:
+      return 'student'
     pass
 
   def link_1_click(self, **event_args):
