612045cf3db88efa29c50ef6832f494b7570b303
esd06007@gmail.com <esd06007@gmail.com>
2024-06-26 16:30:42 +0000

    Edited ServerModule1 and Form2

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -16,3 +16,20 @@ import anvil.server
 #   print("Hello, " + name + "!")
 #   return 42
 #
+import anvil.server
+from anvil import app_tables
+
+@anvil.server.callable
+def get_items():
+    return app_tables.chat.search()
+    
+
+@anvil.server.callable
+def push_update():
+    items = list(app_tables.chat.search())
+    anvil.server.publish('saved_chat_update', items)
+
+@anvil.server.callable
+def add_chat_item(component_data):
+    app_tables.chat.add_row(**component_data)
+    push_update()  # Push update after adding a new item
