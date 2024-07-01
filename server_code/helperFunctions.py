622fab2e76e8433de6ebfaee8ef83fdee5bceb9b
esd06007@gmail.com <esd06007@gmail.com>
2024-07-01 00:15:44 +0000

    Created helperFunctions; Edited settings; Deleted NewSinceLastLogin

--- server_code/helperFunctions.py
+++ server_code/helperFunctions.py
@@ -4,6 +4,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 import time
+import anvil.email
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
@@ -21,3 +22,8 @@ import time
 # 1. I need to query the Users table about the date and time of last login of the user
 # 2. Then I need to see if there are entries in the chat table that are newer than the last login
 # 3. I need to save those entries in a dict to further use them
+# 4. I want to receive an email every time a new message arrived
+
+@anvil.server.callable
+def send_email():
+  anvil.email.send(to="esteban.diaz@uta.edu",from_address='anvil_support',subject='new messages')
